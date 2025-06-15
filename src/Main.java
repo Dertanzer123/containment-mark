f9cc284f3647cbf030e3392454e8bca3ce3888f6
TcePrepK <a.s.arda.simsek@gmail.com>
2025-06-15 22:35:31 +0300

    Spacing + Consistency

--- src/Main.java
+++ src/Main.java
@@ -3,19 +3,16 @@ import core.SystemRoot;
 import java.io.IOException;
 
 public class Main {
-
     public static final String ANSI_RESET = "\u001B[0m";
-
-
     public static final String ANSI_YELLOW = "\033[0;93m";
     public static final String ANSI_BLUE = "\033[0;94m";
-
     public static final String ANSI_RED = "\033[0;91m";
+
     public static void main(String[] args) throws IOException {
-        System.out.println(ANSI_YELLOW+ "Hello, World!"+ ANSI_RESET);
-        System.out.println(ANSI_BLUE+ "Hello, World!"+ ANSI_RESET);
-        System.out.println(ANSI_RED+ "Hello, World!"+ ANSI_RESET);
+        System.out.println(ANSI_YELLOW + "Hello, World!" + ANSI_RESET);
+        System.out.println(ANSI_BLUE + "Hello, World!" + ANSI_RESET);
+        System.out.println(ANSI_RED + "Hello, World!" + ANSI_RESET);
+
         SystemRoot systemRoot = new SystemRoot();
-        System.out.println();
     }
 }
